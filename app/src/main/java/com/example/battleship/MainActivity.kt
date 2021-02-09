c21af5ef850bce1999210cf7fdaa44d83c64d457
Sona Molnarova <molnsona@gmail.com>
2021-02-09 13:02:00 +0100

    Create MainActivity.

--- app/src/main/java/com/example/battleship/MainActivity.kt
+++ app/src/main/java/com/example/battleship/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.battleship
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // get reference to the main button play
+        // set listener (Toast)
+        // and show
+        findViewById<Button>(R.id.btn_main_play).setOnClickListener {
+            Toast.makeText(this, "Game over", Toast.LENGTH_LONG).show()
+        }
     }
 }
