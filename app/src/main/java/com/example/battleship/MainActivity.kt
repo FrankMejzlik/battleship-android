6e154c354a1913f1acb64acc4427c8a18c518071
Sona Molnarova <molnsona@gmail.com>
2021-03-08 16:59:11 +0100

    Remove ViewModelFactory. Substitute with AndroidViewModel.

--- app/src/main/java/com/example/battleship/MainActivity.kt
+++ app/src/main/java/com/example/battleship/MainActivity.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.battleship.viewModels.GameViewModel
-import com.example.battleship.viewModels.GameViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainViewModel: GameViewModel
-    private lateinit var viewModelFactory: GameViewModelFactory
-
 
     fun getViewModel() = mainViewModel
 
@@ -18,9 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewModelFactory = GameViewModelFactory(this.application)
-        mainViewModel = ViewModelProvider(this, viewModelFactory).get(GameViewModel::class.java)
-
+        mainViewModel = ViewModelProvider(this).get(GameViewModel::class.java)
     }
 
     // Suppress back button.
--- app/src/main/java/com/example/battleship/viewModels/GameViewModel.kt
+++ app/src/main/java/com/example/battleship/viewModels/GameViewModel.kt
@@ -1,10 +1,10 @@
 package com.example.battleship.viewModels
 
 import android.app.Application
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.AndroidViewModel
 import com.example.battleship.Game
 
-class GameViewModel(app: Application?) : ViewModel() {
+class GameViewModel(app: Application) : AndroidViewModel(app) {
 
     val game = Game(app)
 }
